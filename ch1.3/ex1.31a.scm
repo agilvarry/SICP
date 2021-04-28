da0e9e3eb6613cfc7740a4fd67bccedd7a45b63a
Alex Gilvarry <alex.gilvarry@gmail.com>
2021-04-28 15:35:17 -0600

    1.3a done

---
+++ ch1.3/ex1.31a.scm
@@ -0,0 +1,27 @@
+;;; The sum procedure is only the simplest of a vast number of similar abstractions that can be captured as higher-order procedures.
+;;; Write an analogous procedure called product that returns the product of the values of a function at points over a given range.
+;;; Show how to define factorial in terms of product. Also use product to compute approximations to using the formula in the book
+
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product term (next a 1) next b))))
+
+(define (inc x y)
+  (+ x 1)
+  )
+
+(define (factor x) x) 
+
+(product factor 1 inc 5)
+;;120
+
+(product factor 1 inc 9)
+;;362880
+
+(define (pi-prod x)
+  (if (even? x)
+      (/ (+ x 2) (+ 1 x))
+      (/ (+ 1 x) (+ x 1))
+  )
