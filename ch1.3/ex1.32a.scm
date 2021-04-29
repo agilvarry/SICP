6fb4b87abb7ee73ad7994f5cee2a535442af2bd2
Alex Gilvarry <alex.gilvarry@gmail.com>
2021-04-28 18:37:38 -0600

    high the details

--- ch1.3/ex1.32a.scm
+++ ch1.3/ex1.32a.scm
@@ -1,30 +1,25 @@
 ;;;a. Show that sum and product are both special cases of a still more general notion called accumulate that combines a collection of terms, using some general accumulation function
 
-(define (product term a next b)
-  (if (> a b)
-      1
-      (* (term a)
-         (product term (next a) next b))))
+(define (inc x) (+ x 1))
 
-(define (sum term a next b)
-  (if (> a b)
-      0
-      (+ (term a)
-         (sum term (next a) next b))))
+(define (identity x) x) 
+
 
 (define (accumulate combiner null-value term a next b)
   (if (> a b)
       null-value
       (combiner (term a)
-         (accumulate combiner null-value term (next a) next b)))
+                (accumulate combiner null-value term (next a) next b)))
   )
-(define (inc x) (+ x 1))
 
-;;identity
-(define (identity x) x) 
 
-;;product/factorial: 120
-(accumulate * 1 identity 1 inc 5)
+(define (product term a next b)
+  (accumulate * 1 term a next b)
+  )
 
-;;sum: 15
-(accumulate + 0 identity 1 inc 5)
+(define (sum term a next b)
+  (accumulate + 0 term a next b)
+  )
+
+(sum identity 1 inc 5)
+(product identity 1 inc 5)
