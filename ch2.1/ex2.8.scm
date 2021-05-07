b0f42159b2a5c9fbd2e2044a1c29f524f9cd2bc5
Alex Gilvarry <alex.gilvarry@gmail.com>
2021-05-07 11:59:30 -0600

    done for real

--- ch2.1/ex2.8.scm
+++ ch2.1/ex2.8.scm
@@ -1,5 +1,11 @@
 ;;;Exercise 2.8.  Using reasoning analogous to Alyssa's, describe how the difference of two intervals may be computed. Define a corresponding subtraction procedure, called sub-interval.
+;;this one i cam up with
+;(define (sub-interval x y)
+;  (make-interval (- (lower-bound x) (upper-bound y))
+;                 (- (upper-bound x) (lower-bound y))))
 
-(define (sub-interval x y)
-  (make-interval (- (lower-bound x) (upper-bound y))
-                 (- (upper-bound x) (lower-bound y))))
+;;this one is intended
+(define (div-interval x y)
+  (add-interval x 
+                (make-interval (- (upper-bound y))
+                               (- (lower-bound y)))))
