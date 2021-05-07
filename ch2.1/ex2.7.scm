8cc8e6d569b82b6781c48125f45f0015347e1785
Alex Gilvarry <alex.gilvarry@gmail.com>
2021-05-07 11:50:31 -0600

    done

---
+++ ch2.1/ex2.7.scm
@@ -0,0 +1,8 @@
+;;;Exercise 2.7.  Alyssa's program is incomplete because she has not specified the implementation of the interval abstraction. Here is a definition of the interval constructor:
+;;;
+;;;(define (make-interval a b) (cons a b))
+;;;
+;;;Define selectors upper-bound and lower-bound to complete the implementation.
+
+(define (upper-bound i) (cdr i))
+(define (lower-bound i) (car i))
