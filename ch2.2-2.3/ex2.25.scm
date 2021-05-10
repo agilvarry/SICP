e28c2f087b3d40e3a35c1f70816ce5659c23dfda
Alex Gilvarry <alex.gilvarry@gmail.com>
2021-05-10 15:09:46 -0600

    done

---
+++ ch2.2-2.3/ex2.25.scm
@@ -0,0 +1,9 @@
+;;;Exercise 2.25.  Give combinations of cars and cdrs that will pick 7 from each of the following lists
+
+(car (cdaddr '(1 3 (5 7) 9)))
+
+(caar '((7)))
+
+(cadadr (cadadr (cadadr '(1 (2 (3 (4 (5 (6 7)))))))))
+
+
