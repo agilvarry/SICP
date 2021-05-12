b99f201067c47383339f392a67d077590b60d47e
Alex Gilvarry <alex.gilvarry@gmail.com>
2021-05-12 08:06:48 -0600

    done

--- ch2.2-2.3/ex2.36.scm
+++ ch2.2-2.3/ex2.36.scm
@@ -14,7 +14,7 @@
 (define (accumulate-n op init seqs)
   (if (null? (car seqs))
       '()
-      (cons (accumulate op init (map (lambda (seq) (op init (car seq))) seqs))
-            (accumulate-n op init (map (lambda (seq) (cdr seq)) seqs)))))
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
 
 (accumulate-n + 0 '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
