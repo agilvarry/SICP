67b8bf340acb24b4fd9ac8fe13cfa5119249aeb5
Alex Gilvarry <alex.gilvarry@gmail.com>
2021-05-16 10:30:28 -0600

    done

---
+++ ch2.4-2.5/ex2.79.scm
@@ -0,0 +1,18 @@
+;;;Exercise 2.79.  Define a generic equality predicate equ? that tests the equality of two numbers, and install it in the generic arithmetic package. 
+;;;This operation should work for ordinary numbers, rational numbers, and complex numbers. 
+
+(put 'equ? '(scheme-number1 scheme-number2)
+     (lambda (x y) (= x y)))
+
+(define (equ-rat? x y)
+  (and (= (denom x) (denom y))
+       (=  (num x) (num y))))
+
+(put 'equ? '(rat1 rat2) equ-rat?)
+
+
+(define (equ-comp? x y)
+    (and (= (real-part x) (real-part y))
+	 (= (imag-part x) (imag-part y))))
+
+(put 'equ? '(imag1 imag2) equ-comp?)
