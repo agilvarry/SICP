61203fe3ab8bdd89bf2360c1ecc38041ea26571d
Alex Gilvarry <alex.gilvarry@gmail.com>
2021-05-26 17:07:30 -0600

    done

---
+++ ch3.3/ex3.12.scm
@@ -0,0 +1,20 @@
+;;;Exercise 3.13
+
+(define (append x y)
+  (if (null? x)
+      y
+      (cons (car x) (append (cdr x) y))))
+
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))      
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+(define z (append x y))
+z
