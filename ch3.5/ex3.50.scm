3145cdad02f56bb402fae90fb2a4d9f9f059cdf4
Alex Gilvarry <alex.gilvarry@gmail.com>
2021-06-01 15:41:11 -0600

    done

---
+++ ch3.5/ex3.50.scm
@@ -0,0 +1,10 @@
+;;;Exercise 3.50.  Complete the following definition, which generalizes stream-map to allow procedures that take multiple arguments, 
+;;;analogous to map in section 2.2.3, footnote 12.
+
+(define (stream-map proc . argstreams)
+  (if (steam-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc (map stream-cdr argstreams))))))
