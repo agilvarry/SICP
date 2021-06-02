73d89c9bf98e9dfa1411160ce02d2ea77b000ae9
Alex Gilvarry <alex.gilvarry@gmail.com>
2021-06-02 12:28:54 -0600

    done

---
+++ ch3.5/ex3.64.scm
@@ -0,0 +1,14 @@
+;;;Exercise 3.64.  Write a procedure stream-limit that takes as arguments a stream and a number (the tolerance). 
+;;;It should examine the stream until it finds two successive elements that differ in absolute value by less than the tolerance, 
+;;;and return the second of the two elements. Using this, we could compute square roots up to a given tolerance by:
+
+(define (sqrt x tolerance)
+  (stream-limit (sqrt-stream x) tolerance))
+
+(define (steam-limit s tolerance)
+    (if (< (- (stream-ref s 1) (stream-ref s 0)) tolerance)
+      (stream-ref s 1)
+      (stream-limit (stream-cdr s) tolerance)
+    )
+)
+
